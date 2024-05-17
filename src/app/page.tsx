6bfdf96db12c4ce3686361ead5bf294fcf4929ef
muhammadhashim <muhammadhashim2133@gmail.com>
2024-05-17 22:25:48 +0500

    features section completed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,14 @@
 import Header from "@/widgets/Header";
 import Footer from "@/widgets/Footer";
 import HeroSection from "@/widgets/HeroSection";
+import FeaturesSection from "@/widgets/FeaturesSection";
+
 export default function Home() {
   return (
     <main>
       <Header />
       <HeroSection />
+      <FeaturesSection />
       {/*      <Footer /> */}
 
     </main>
