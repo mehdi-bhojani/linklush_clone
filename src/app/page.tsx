7f75e3e779bb07c5fcbeabf592b28dc6100bd097
muhammadhashim <muhammadhashim2133@gmail.com>
2024-05-17 16:33:06 +0500

    header and footer are completed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,12 @@
-import { Button } from "@/components/ui/button";
-
-
+import Header from "@/widgets/Header";
+import Footer from "@/widgets/Footer";
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div>hello world</div>
-      <Button>click</Button>
-
-      <div className="bg-white dark:bg-gray-800">
-        <p className="text-gray-800 dark:text-gray-200">
-          This text is black in light mode and light gray in dark mode.
-        </p>
-        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded dark:bg-blue-700 dark:hover:bg-blue-900">
-          Button
-        </button>
+    <main className="flex min-h-screen flex-col items-center justify-between ">
+      <Header />
+      <div className="container">
+        <Footer />
       </div>
-
     </main>
   );
 }
