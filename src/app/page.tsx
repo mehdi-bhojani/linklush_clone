4346e0793b422e479c4af0ff62a716e5f8d41a59
muhammadhashim <muhammadhashim2133@gmail.com>
2024-05-18 11:54:18 +0500

    integration section completed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ import HeroSection from "@/widgets/HeroSection";
 import FeaturesSection from "@/widgets/FeaturesSection";
 import GrowAudienceSection from "@/widgets/GrowAudienceSection";
 import UnderstandAudienceSection from "@/widgets/UnderstandAudienceSection";
+import IntegrationSection from "@/widgets/IntegrationSection";
 
 export default function Home() {
   return (
@@ -13,6 +14,7 @@ export default function Home() {
       <FeaturesSection />
       <GrowAudienceSection />
       <UnderstandAudienceSection />
+      <IntegrationSection />
       {/*      <Footer /> */}
 
     </main>
