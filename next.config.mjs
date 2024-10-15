0199d9f979d13b98b19a3f6375d2c064ce9125ac
mehdi-bhojani <m.mehdibhojani1@gmail.com>
2024-10-15 16:45:26 +0500

    initial commit

--- next.config.mjs
+++ next.config.mjs
@@ -2,31 +2,41 @@
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-      loader: 'default',
-      domains: [], // This can be left empty
-      remotePatterns: [
-        {
-          protocol: 'https',
-          hostname: '**', // Allow all hostnames
-        },
-      ],
-    },
-    async headers() {
-      return [
-        {
-          // matching all API routes
-          source: "/:path*",
-          headers: [
-            { key: "Access-Control-Allow-Credentials", value: "true" },
-            { key: "Access-Control-Allow-Origin", value: "*" }, // replace this with your actual origin
-            { key: "Access-Control-Allow-Methods", value: "GET,DELETE,PATCH,POST,PUT" },
-            { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
-          ],
-        },
-      ];
-    },
-  };
-  
-  export default nextConfig;
-  
+  images: {
+    loader: "default",
+    domains: [],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**", // Allow all hostnames
+      },
+    ],
+  },
+  experimental: {
+    missingSuspenseWithCSRBailout: false,
+  }, // This can be left empty
+
+  async headers() {
+    return [
+      {
+        // matching all API routes
+        source: "/:path*",
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          { key: "Access-Control-Allow-Origin", value: "*" }, // replace this with your actual origin
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET,DELETE,PATCH,POST,PUT",
+          },
+          {
+            key: "Access-Control-Allow-Headers",
+            value:
+              "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
+          },
+        ],
+      },
+    ];
+  },
+};
+
+export default nextConfig;
